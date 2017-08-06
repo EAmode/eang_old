1e1ffde43aaefc415a95b5f44957cbf1c705f2d3
Sebastian Kropp <sebastian@kropponline.de>
2017-08-06 14:06:31 -0400

    fixed rollup dependencies

--- rollup-external.js
+++ rollup-external.js
@@ -2,7 +2,12 @@ export const external = [
   '@angular/core',
   '@angular/common',
   '@angular/forms',
+  'rxjs/Observable',
+  'rxjs/add/observable/of',
+  'rxjs/add/observable/combineLatest',
   'rxjs/add/operator/debounceTime',
   'rxjs/add/operator/distinctUntilChanged',
   'rxjs/add/operator/switchMap',
+  'markdown-it',
+  'lodash'
 ]
