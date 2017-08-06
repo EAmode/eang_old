62a54d85d0d43bed58fcb25749d63f5cf0a22869
Sebastian Kropp <sebastian@kropponline.de>
2017-08-06 12:33:53 -0400

    lodash templates

--- src/components/markdown.ts
+++ src/components/markdown.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, Output, EventEmitter, Input, TemplateRef, ContentChild } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter, Input, TemplateRef, ContentChild, OnDestroy } from '@angular/core'
 import { FormControl } from '@angular/forms'
 import { Observable } from 'rxjs/Observable'
+import { Subscription } from 'rxjs/Subscription'
+import * as MarkdownIt from 'markdown-it'
+import * as _ from 'lodash'
+import 'rxjs/add/observable/combineLatest'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
 import 'rxjs/add/operator/switchMap'
@@ -8,84 +12,29 @@ import 'rxjs/add/operator/switchMap'
 @Component({
   selector: 'ea-markdown',
   template: `
-<!-- MQ(S) flex-row-holder -->
-<div class="ea-card card-full md-flex-column-holder" style="min-height: 300px;">
-    <!-- MQ(S) .card-full { order: 1 } -->
-  <div *ngIf="editing" class="md-card-w">
-    <div class="ea-card-header">
-      <h3>EA Markdown Editor</h3>
-      <div>
-        <button md-menu-item *ngIf="editing" class="flex-row-holder" (click)="save()"> <img src="../assets/save.svg" alt="save"></button>
-      </div>
-    </div>
-    <div class="flex-column-holder hundo">
-      <textarea name="textarea" [(ngModel)]="changedContent"></textarea>
-    </div>
-  </div>
-  <!-- MQ(S)  md-editor { width: 100% } {overflow-x: scroll} -->
-  <div class="md-editor card-full">
-    <div class="textera-position">
-      <div class="ea-card-header">
-        <div class="title">
-          <h3>{{model.node.name}}</h3>
-        </div>
-        <div>
-          <button md-icon-button [mdMenuTriggerFor]="menu">
-          <md-icon>more_vert</md-icon>
-         </button>
-          <md-menu #menu="mdMenu">
-              <button md-menu-item *ngIf="!editing" (click)="edit()"> <div class="logo-edit"></div> Edit </button>
-              <button md-menu-item (click)="edit()"> <i class="material-icons">open_in_new</i> Open </button>
-
-          </md-menu>
-        </div>
-      </div>
-      <div class="md-content" [innerHTML]="renderedModel"></div>
-    </div>
-  </div>
-</div>`
+    <div class="md-content" [innerHTML]="compiledMarkdown"></div>
+  `
 })
-export class MarkdownComponent implements OnInit {
-  private md = new MarkdownIt({ html: true })
+export class MarkdownComponent implements OnDestroy {
+  markdownIt = new MarkdownIt({ html: true })
 
-  @Input() model
+  @Input() doc: Observable<string>
+  @Input() ctx: Observable<any>
+  subscription: Subscription
 
-  renderedModel
-  changedContent: string
-  editing = false
+  compiledMarkdown: string
 
-  constructor(private mode: ModeService) { }
-
-  ngDoCheck() {
-    this.render()
-  }
-
-  edit() {
-    this.editing = true
-    this.changedContent = this.model.node.content
-  }
-
-  save() {
-    this.editing = false
-    this.model.node.content = this.changedContent
-    this.mode.objectModify(this.model.node).subscribe(
-      x => {
-        console.log(x)
-      },
-      error => {
-        console.log(error)
-        //this.errorMessage = error.toString()
+  constructor() {
+    this.subscription = Observable
+      .combineLatest(this.doc, this.ctx, (doc, ctx) => ({ doc, ctx }))
+      .subscribe(change => {
+        const compiled = _.template(change.doc)
+        this.markdownIt.render(compiled(change.ctx))
       })
   }
 
-  private render() {
-    if (this.editing && this.changedContent) {
-      this.renderedModel = this.md.render(this.changedContent)
-    } else {
-      if (this.model && this.model.node.content) {
-        this.renderedModel = this.md.render(this.model.node.content)
-        //this.renderedModel = new String(this.model.object.content)
-      }
-    }
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms'
+
 import { AutocompleteComponent } from './components/autocomplete'
 import { MenuTreeComponent } from './components/menu-tree'
+import { MarkdownComponent } from './components/markdown'
 
 export * from './components/autocomplete'
 export * from './components/menu-tree'
+export * from './components/markdown'
 
 @NgModule({
-  declarations: [AutocompleteComponent, MenuTreeComponent],
-  exports: [AutocompleteComponent, MenuTreeComponent],
+  declarations: [
+    AutocompleteComponent,
+    MenuTreeComponent,
+    MarkdownComponent],
+  exports: [
+    AutocompleteComponent,
+    MenuTreeComponent,
+    MarkdownComponent],
   imports: [CommonModule, ReactiveFormsModule]
 })
 export class EangModule { }
