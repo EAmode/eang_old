a154f09c58787d40e7e310ef7321fd0d0988aad0
Sebastian Kropp <sebastian@kropponline.de>
2017-08-29 13:58:53 -0400

    augmentation

--- src/services/state-machine.ts
+++ src/services/state-machine.ts
@@ -11,7 +11,15 @@ export class StateMachineService {
   currentTransitions$: Subject<any>
   currentState$: Subject<any>
   currentState: any
-  constructor(public states, public transitionDefs, public initialState) {
+  constructor(public states: StateMap, public transitionDefs, public initialState: State) {
+    for (const key in states) {
+      if (states.hasOwnProperty(key)) {
+        const s = states[key]
+        if (s.name === undefined) {
+          s.name = _.startCase(key)
+        }
+    }
+    }
     this.currentState = initialState
     this.currentTransitions$ = new BehaviorSubject(this.currentTransitions())
     this.currentState$ = new BehaviorSubject(initialState)
@@ -37,4 +45,46 @@ export class StateMachineService {
 
   }
 
+  setData(data) {
+
+  }
 }
+
+export interface State {
+  name: string,
+  count: number,
+  order: number,
+  valid: boolean,
+  data?: any
+}
+
+export interface StateMap {
+  [index: string]: State
+  [index: number]: State
+}
+
+export interface Transition {
+  name: string,
+  failingGuards: Array<Guard>
+  fromState: State,
+  toState: State,
+  transitionDef: TransitionDefinition
+}
+
+export interface TransitionDefinition {
+  name: string,
+  order: number,
+  valid: boolean,
+  data?: any,
+  from(currentState: State): Array<State>,
+  to(currentState: State): Array<State>,
+}
+
+export interface Guard {
+  (allStates: string,
+    to: number,
+    order: number,
+    valid: boolean,
+    data?: any)
+}
+
