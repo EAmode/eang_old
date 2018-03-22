cac864705150d111a38b3e3c4ee485f9839f3902
Sebastian Kropp <sebastian@kropponline.de>
2018-03-22 19:43:25 -0400

    chore: formating

--- playground/systemjs.config.js
+++ playground/systemjs.config.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 /**
  * System configuration for Angular samples
  * Adjust as necessary for your application needs.
  */
-(function () {
+;(function() {
   System.config({
     paths: {
       // paths serve as alias
@@ -18,18 +18,21 @@
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/platform-browser':
+        'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic':
+        'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
       // other libraries
       rxjs: 'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'eang': '../dist/eang.umd.js',
-      "lodash": 'npm:lodash/lodash.js',
-      "markdown-it": 'npm:markdown-it/dist/markdown-it.js',
+      'angular-in-memory-web-api':
+        'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      eang: '../dist/eang.umd.js',
+      lodash: 'npm:lodash/lodash.js',
+      'markdown-it': 'npm:markdown-it/dist/markdown-it.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -45,5 +48,5 @@
         defaultExtension: 'js'
       }
     }
-  });
-})(this);
+  })
+})(this)
