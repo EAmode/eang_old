ad3e582ad0fa65217d997423621f3c418907c398
Sebastian Kropp <sebastian@kropponline.de>
2018-03-23 00:31:12 -0400

    fix(panel): allow attributes to be string as well as observables

--- src/components/panel.ts
+++ src/components/panel.ts
@@ -10,12 +10,19 @@ import { of } from 'rxjs/observable/of'
   template: `<div id="ea-panel-container" [class]="classNames | async"><ng-content></ng-content></div>`
 })
 export class PanelComponent implements OnInit {
-  @Input() state = of('maximized')
-  @Input() orientation = of('left')
+  @Input() state
+  @Input() orientation
   classNames: Observable<string>
   stateName: any
 
   ngOnInit(): void {
+    if (typeof this.state === 'string') {
+      this.state = of(this.state)
+    }
+    if (typeof this.orientation === 'string') {
+      this.orientation = of(this.orientation)
+    }
+
     this.classNames = combineLatest(this.state, this.orientation).pipe(
       map(([state, orientation]) => `${state} ${orientation}`)
     )
