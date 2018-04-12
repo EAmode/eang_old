16f582249874e9e6a6ad10ef4fd42c77609d51c1
Sebastian Kropp <sebastian@kropponline.de>
2018-04-11 23:44:32 -0400

    fix(panel): wrong input

--- playground/app/panel.ts
+++ playground/app/panel.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core'
+import { Observable } from 'rxjs/Observable'
+import { Subject } from 'rxjs/Subject'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 
 @Component({
   selector: 'pg-panel',
-  templateUrl: './panel.html'
+  templateUrl: './panel.html',
+  styleUrls: ['./panel.css']
 })
 export class PanelPageComponent {
+  panelState = new BehaviorSubject<string>('maximized')
   constructor() {}
+
+  changeState(state: string) {
+    this.panelState.next(state)
+  }
 }
