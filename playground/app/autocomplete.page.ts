7809633d9796cd5f42a1077250a0edbff98240b9
Sebastian Kropp <sebastian@kropponline.de>
2018-05-16 23:46:33 -0400

    chore(playground): added avatars

--- playground/app/autocomplete.page.ts
+++ playground/app/autocomplete.page.ts
@@ -11,6 +11,37 @@ import { of } from 'rxjs/observable/of'
 export class AutocompletePageComponent {
   results = of([])
 
+  avatars = of([
+    {
+      name: 'Han Solo',
+      avatarUrl: 'https://api.adorable.io/avatars/140/1.png'
+    },
+    {
+      name: 'Darth Vader',
+      avatarUrl: 'https://api.adorable.io/avatars/140/2.png'
+    },
+    {
+      name: 'Luke Skywalker',
+      avatarUrl: 'https://api.adorable.io/avatars/140/3.png'
+    },
+    {
+      name: 'Princess Leia',
+      avatarUrl: 'https://api.adorable.io/avatars/140/4.png'
+    },
+    {
+      name: 'Obi Wan',
+      avatarUrl: 'https://api.adorable.io/avatars/140/5.png'
+    },
+    {
+      name: 'Hulk',
+      avatarUrl: 'https://api.adorable.io/avatars/140/6.png'
+    },
+    {
+      name: 'Lando Clarissian',
+      avatarUrl: 'https://api.adorable.io/avatars/140/7.png'
+    }
+  ])
+
   constructor(private http: HttpClient) {
     this.results.subscribe(i => {
       console.log(i)
@@ -24,14 +55,18 @@ export class AutocompletePageComponent {
   }
 
   onNameSearch(name: string) {
-    this.http
-      .get(`uzby.com/api.php?min=3&max=8`, {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      })
-      .subscribe(data => {
-        console.log(data)
-      })
+
+    const result = this.avatars.filter(a => a.name.startsWith(name))
+    this.results = of(result)
+    console.log(name)
+    // this.http
+    //   .get(`uzby.com/api.php?min=3&max=8`, {
+    //     headers: new HttpHeaders({
+    //       'Content-Type': 'application/json'
+    //     })
+    //   })
+    //   .subscribe(data => {
+    //     console.log(data)
+    //   })
   }
 }
