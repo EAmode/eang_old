e26ed195c62381060bbbf1de167b43e9f164f46c
Sebastian Kropp <sebastian@kropponline.de>
2018-07-13 15:37:49 -0400

    postcss integration

---
+++ apps/eang-io/postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    require('postcss-import')(),
+    require('postcss-nested')(),
+    require('postcss-cssnext')({
+      features: {
+        customProperties: {
+          warnings: false
+        }
+      }
+    }),
+    require('postcss-assets')(),
+    require('lost')()
+  ]
+}
