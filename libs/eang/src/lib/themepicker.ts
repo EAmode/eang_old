986c3b79d880ff792990c6aae0a35760116bcb37
Sebastian Kropp <sebastian@kropponline.de>
2018-07-20 01:54:48 -0400

    added theme picker

--- libs/eang/src/lib/themepicker.ts
+++ libs/eang/src/lib/themepicker.ts
@@ -1,49 +1,42 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core'
 
 @Component({
-    selector: 'ea-themepicker',
-    template: `<button class="icon-button" [ngStyle]="{'background-image': 'url(assets/images/palette.svg)'}" (click)="toggleThemes()">
+  selector: 'ea-themepicker',
+  template: `<button class="icon-button" [ngStyle]="{'background-image': 'url(assets/images/palette.svg)'}" (click)="toggleThemes()">
   </button>
-  <div *ngIf="showThemes">
-    <ul class="eang-theme-dropdown-list">
-      <li *ngFor="let theme of themes" (click)="selectTheme(theme)" class="eang-theme-dropdown-element">{{theme}}</li>
+  <div *ngIf="showThemes" class="ea-themepicker-dropdown">
+    <ul class="ea-themepicker-dropdown-list">
+      <li *ngFor="let theme of themes" (click)="selectTheme(theme)" class="ea-themepicker-list-item">{{theme}}</li>
     </ul>
   </div>
   `
 })
 export class ThemePickerComponent implements OnInit {
-    @Input() themes
-    @Input() select
-    themeMatch = []
-    showThemes
+  @Input() themes
+  @Input() select
+  themeMatch = []
+  showThemes = false
 
-    constructor() {
-        this.showThemes = false
-    }
+  ngOnInit() {
+    this.themes.forEach(element => {
+      this.themeMatch.push(element)
+    })
+  }
 
-    ngOnInit() {
-        this.themes.forEach(element => {
-            this.themeMatch.push(element)
-        })
-    }
-
-    selectTheme(theme) {
-        const themedElements = document.querySelectorAll(this.select)
-        Array.from(themedElements).forEach((element: HTMLElement) => {
-            this.themeMatch.forEach(match => {
-                if (element.classList.contains(match)) {
-                    element.classList.remove(match)
-                }
-            })
-            element.classList.add(theme)
-        })
-    }
-
-    toggleThemes() {
-        if (this.showThemes) {
-            this.showThemes = false
-        } else {
-            this.showThemes = true
+  selectTheme(theme) {
+    const themedElements = document.querySelectorAll(this.select)
+    Array.from(themedElements).forEach((element: HTMLElement) => {
+      this.themeMatch.forEach(match => {
+        if (element.classList.contains(match)) {
+          element.classList.remove(match)
         }
-    }
+      })
+      element.classList.add(theme)
+    })
+    this.showThemes = false
+  }
+
+  toggleThemes() {
+    this.showThemes = !this.showThemes
+  }
 }
