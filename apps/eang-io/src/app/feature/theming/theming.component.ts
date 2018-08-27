7dda76d42d348ef1d94fc02fcd1a5e18704a6981
Will Dyess <ea@Wills-MBP.fios-router.home>
2018-08-27 13:12:35 -0400

    new theme | updated button

--- apps/eang-io/src/app/feature/theming/theming.component.ts
+++ apps/eang-io/src/app/feature/theming/theming.component.ts
@@ -95,29 +95,59 @@ export class ThemingComponent implements OnInit {
   `
 
   component = `
+  ## Compose-ability
+
+  The ability to override specific aspects of a component, and to default to base eang value is made easier with custom properties. Below is an example of the ea-card component and how it can be easily configured to have differing colors and backgrounds for the ea-card-header and the ea-card-footer.
+
+  *card.css*
   ~~~ css
+    .ea-card {
+      --ea-card-color: var(--ea-card-color, var(--ea-color-1));
+      --ea-card-background: var(--ea-card-background, var(--ea-color-3));
+      .
+      .
+    }
+  ~~~
+The default values for the ea-card are the *--ea-card-color* and the *--ea-card-background*.
 
-  ea-panel {
-    --ea-panel-color: var(--ea-color-1);
-    --ea-panel-background: var(--ea-color-background-2);
-    --ea-panel-height: var(--ea-panel-height, initial);
-    --ea-panel-width: var(--ea-panel-width, initial);
-    --ea-panel-max-height: var(--ea-panel-max-height, 100%);
-    --ea-panel-max-width: var(--ea-panel-max-width, 100%);
-    --ea-panel-min-size-ratio: 0.5;
+~~~ css
+.ea-card {
+  .
+  .
+  --ea-card-header-color: var(--ea-card-header-color, var(--ea-card-color));
+  --ea-card-header-background: var(--ea-card-header-background, var(--ea-card-background));
+}
+~~~
+The way it is written, unless --ea-card-header-color is defined, the header color will default to being the same color that is defined for the --ea-card-color. The same method is applied to the --ea-card-header-background.
+  ~~~ css
+  .mode {
+    & .ea-card {
+      --ea-card-color: var(--ea-card-color, var(--ea-color-1));
+      --ea-card-background: var(--ea-card-background, var(--ea-color-3));
 
-    display: block;
-    color: var(--ea-panel-color);
-    background: var(--ea-panel-background);
-    height: var(--ea-panel-height);
-    width: var(--ea-panel-width);
+      --ea-card-header-color: var(--ea-card-header-color, var(--ea-card-color));
+      --ea-card-header-background: var(--ea-card-header-background, var(--ea-card-background));
+
+      --ea-card-footer-color: var(--ea-card-footer-color, var(--ea-card-header-color, var(--ea-card-color)));
+      --ea-card-footer-background: var(--ea-card-footer-background , var(--ea-card-header-background, var(--ea-card-background)));
+
+      color: var(--ea-card-color);
+      background: var(--ea-card-background);
+
+      & .ea-card-header {
+        color: var(--ea-card-header-color);
+        background: var(--ea-card-header-background);
+      }
+      & .ea-card-content {
+        color: var(--ea-card-content-color);
+        background: var(--ea-card-content-background);
+      }
+      & .ea-card-footer {
+        color: var(--ea-card-footer-color);
+        background: var(--ea-card-footer-background);
+      }
+    }
   }
-
-  & ea-panel[state='minimized'] {
-    --ea-panel-height: calc(
-      var(--ea-panel-max-height) * var(--ea-panel-min-size-ratio)
-    );
-
   ~~~
   `
 
