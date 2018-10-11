e3205f2da85d9bee7e5b03d37fe422b6e0a28af6
Sebastian Kropp <sebastian@kropponline.de>
2018-10-11 16:36:22 -0400

    fix(toolbar): moved button to toolbar header

--- projects/eamode/eang/src/components/toolbar.ts
+++ projects/eamode/eang/src/components/toolbar.ts
@@ -12,11 +12,19 @@ import {
 } from '@angular/core'
 import { Observable, Subscription } from 'rxjs'
 import { debounceTime, distinctUntilChanged, map, delay } from 'rxjs/operators'
+import { LayoutService, DrawerState } from '../services/layout.service'
 
 @Component({
   selector: 'ea-toolbar',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
+  <ng-container *ngIf="(layout.drawerState$ | async) as drawerState">
+  <div class="ea-icon-button" (click)="layout.drawerState$.next(DrawerState.maximized)"
+        [class.menu-close]="drawer"
+        [class.menu-open]="!drawer">
+        </div>
+        {{drawerState}}
+</ng-container>
   <ng-content select="[ea-toolbar-header]"></ng-content>
   <ng-content select="[ea-toolbar-action]"></ng-content>
   <ng-content select="[ea-toolbar-buttons]"></ng-content>
@@ -35,7 +43,7 @@ export class Toolbar implements OnInit, OnDestroy {
   @ViewChild('suggestionPanel') suggestionPanel
   @ContentChild(TemplateRef) resultsTemplate: TemplateRef<any>
 
-  constructor() {}
+  constructor(public layout: LayoutService) {}
 
   ngOnInit() {}
 
