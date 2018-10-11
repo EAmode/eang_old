95cb5e7f0dacc0cf6db06198540639c6658b21b3
Sebastian Kropp <sebastian@kropponline.de>
2018-10-10 23:55:45 -0400

    build(themes): added style linting

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  rules: {
+    'selector-nested-pattern': '(&|>)',
+    'selector-type-no-unknown': [true, {
+      ignoreTypes: ["/^ea-/"]
+    }]
+  }
+}
