3a65dce06359763d4239dfb54685471bd0947f6e
Will Dyess <ea@Wills-MacBook-Pro.local>
2018-12-15 14:13:15 -0500

    docs(vars): vars update

--- src/app/variables/variables.component.ts
+++ src/app/variables/variables.component.ts
@@ -11,42 +11,24 @@ export class VariablesComponent implements OnInit {
   isActive = false
   textIsActive = false
 
-  // setActive() {
-  //   this.isActive = !this.isActive
-  // }
-
   ngOnInit() {}
-  variables_1 = `
-  ~~~css
-  here are som vars
-  ~~~
-  `
-  variables_code = `
-  ~~~html
-    <div style="background: var(--ea-base-color);"></div>
-    <div style="background: var(--ea-base-color);"></div>
-    <div style="background: var(--ea-base-color);"></div>
-    <div style="background: var(--ea-base-color);"></div>
-    <div style="background: var(--ea-base-color);"></div>
-  ~~~
-  `
 
   variables = `
-  |  Title   |  Custom Property  | Description | rgba |
+  |  Title   |  Custom Property  | Description | hsl( ) |
   |---|---|---|---|
-  |  Base Color       | \` --ea-base-color \` | this would set the panel to its closed state | 1.2 14 |
-  |  Base Color High   |  \` --ea-base-color-high \` |this would set the panel to its maximized, or open, state   |  --ea-base-color |
-  |  Alt Base Color   |  \` --ea-alt-base-color \` |this would set the panel to its maximized, or open, state   |  --ea-base-color |
-  |  Background Hover |  \` --ea-background-color-hover \` |this would set the panel to its maximized, or open, state   |  --ea-base-color |
-  |  Color Hover      |  \` --ea-background-color-active \` | maximized, or open, state   |  --ea-base-color |
+  |  Base Color       | \` --ea-base-color \` | Main theme color for components | hsl(223, 40%,43%) |
+  |  Base Color High   |  \` --ea-base-color-high \` | 10% increase from --ea-base-color in lightness for states|hsl(223, 40%, 53%)|
+  |  Alt Base Color   |  \` --ea-alt-base-color \` | Secondary color for components |hsl(0, 0%, 20%)|
+  |  Background Color Hover |  \` --ea-background-color-hover \` |Background color for all hover state components|\`var(--ea-base-color)\`|
+|Background Color Active|\` --ea-background-color-active \`|Background color for all active state components|\`var(--ea-base-color-high)\`|
   `
 
   text_variables = `
   |  Title   |  Custom Property  | Description | rgba |
   |---|---|---|---|
-  |  Text Color Dark |  \` --ea-text-color-light \` |this would set the panel to its maximized, or open, state   |  --ea-base-color |
-  |  Text Color Light |  \`--ea-text-color-dark \` |this would set the panel to its maximized, or open, state   |  --ea-base-color |
-  |  Text Color Hover      |  \` --ea-text-color-hover \` | maximized, or open, state   |  --ea-base-color |
-  |  Text Color Active      |  \` --ea-text-color-active \` | maximized, or open, state   |  --ea-base-color |
+  |  Text Color Dark |  \` --ea-text-color-light \` |Light color text for the theme|hsl(0, 0%, 96%)|
+  |  Text Color Light |  \`--ea-text-color-dark \` |Dark color text for the theme|hsl(0, 0%, 20%)|
+  |  Text Color Hover      |  \` --ea-text-color-hover \` |Text color for all hover state components|  \`var(--ea-text-color-light)\` |
+  |  Text Color Active      |  \` --ea-text-color-active \` |Text color for all active state components| \` --ea-text-color-light\`|
   `
 }
