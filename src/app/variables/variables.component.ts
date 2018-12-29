3cba98534615489c78f60f758a05d83da9504516
Will Dyess <ea@Wills-MBP.home>
2018-12-28 21:36:43 -0500

    feat(updated variables): convention the same across all variables
    BREAKING CHANGE: base component names have been changed, some globally defined variables may not
    work any longer or will need to be updated

--- src/app/variables/variables.component.ts
+++ src/app/variables/variables.component.ts
@@ -17,10 +17,10 @@ export class VariablesComponent implements OnInit {
   |  Title   |  Custom Property  | Description | hsl( ) |
   |---|---|---|---|
   |  Base Color       | \` --ea-color-1 \` | Main theme color for components | hsl(223, 40%,43%) |
-  |  Base Color High   |  \` --ea-color-1-high \` | 10% increase from --ea-color-1 in lightness for states|hsl(223, 40%, 53%)|
+  |  Base Color High   |  \` --ea-color-1-light \` | 10% increase from --ea-color-1 in lightness for states|hsl(223, 40%, 53%)|
   |  Alt Base Color   |  \` --ea-color-2 \` | Secondary color for components |hsl(0, 0%, 20%)|
   |  Background Color Hover |  \` --ea-background-color-hover \` |Background color for all hover state components|\`var(--ea-color-1)\`|
-|Background Color Active|\` --ea-background-color-active \`|Background color for all active state components|\`var(--ea-color-1-high)\`|
+|Background Color Active|\` --ea-background-color-active \`|Background color for all active state components|\`var(--ea-color-1-light)\`|
   `
 
   text_variables = `
