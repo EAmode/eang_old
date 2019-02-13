b9fc81e508915f59f688b853216ef4b626187ac7
Will Dyess <ea@Wills-MacBook-Pro.local>
2019-02-13 00:30:22 -0500

    docs(style docs): style guide

--- src/app/utilities/utilities.component.ts
+++ src/app/utilities/utilities.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core'
   styles: []
 })
 export class UtilitiesComponent implements OnInit {
+  center = `
+  ~~~html
+  <div center>
+  </div>
+  ~~~
+  `
+
+  multi_col = `
+  ~~~html
+  <div multi-col style="--ea-grid-col-min-width: 18em;">
+    <div center>testing</div>
+    <div center>testing</div>
+  </div>
+  ~~~
+  `
   constructor() {}
 
   ngOnInit() {}
