71e769cbf6c0cb2ca71daf2b6b2a9fa3cdc4aa03
Alexander Romaniak <sanya.romanyak@gmail.com>
2019-07-11 17:05:35 +0300

    feat(table): copy code to cpilboard

--- src/app/feature/table/table.component.ts
+++ src/app/feature/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Renderer2 } from '@angular/core'
+import { MarkdownComponent } from '@eamode/eang'
+import { timer, interval } from 'rxjs'
 
 class TableItem {
   constructor(public technology: string, public experience: string) {}
@@ -10,6 +12,7 @@ class TableItem {
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  public showSuccessfullyCopied = false
   tableData = [
     new TableItem('C++', '5 years'),
     new TableItem('Pascal', '10 years'),
@@ -19,11 +22,29 @@ export class TableComponent implements OnInit {
   constructor(public renderer2: Renderer2) {}
 
   onRowSelect(row: HTMLElement) {
-    if (row.getAttribute('active')) {
-      this.renderer2.removeAttribute(row, 'active')
+    if (row.getAttribute('selected')) {
+      this.renderer2.removeAttribute(row, 'selected')
     } else {
-      this.renderer2.setAttribute(row, 'active', 'true')
+      this.renderer2.setAttribute(row, 'selected', 'true')
     }
+  }
+
+  copyCode(code: MarkdownComponent) {
+    this.showInfoAboutSuccessfullyCopy()
+
+    const el = document.createElement('textarea')
+    el.value = code.doc
+    document.body.appendChild(el)
+    el.select()
+    document.execCommand('copy')
+    document.body.removeChild(el)
+  }
+
+  showInfoAboutSuccessfullyCopy() {
+    this.showSuccessfullyCopied = true
+    timer(2000).subscribe(() => {
+      this.showSuccessfullyCopied = false
+    })
   }
 
   selectableTable = `
@@ -121,31 +142,25 @@ export class TableComponent implements OnInit {
   ~~~
   `
 
-  stripedNHoveredTable = `
+  stripedNHoveredNSelectableTable = `
   ~~~html
   <table class="ea-table">
-  <caption>Program languages experience</caption>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Experience</th>
-        </tr>
-      </thead>
-      <tbody striped table-hover>
-      <tr>
-          <td>C++</td>
-          <td>5 years</td>
-      </tr>
-      <tr>
-          <td>Pascal</td>
-          <td>10 Years</td>
-      </tr>
-      <tr>
-        <td>Assembler</td>
-        <td>15 years</td>
-      </tr>
-      </tbody>
-    </table>
+  <caption>
+    Table 1. Programming languages experience
+  </caption>
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Experience</th>
+    </tr>
+  </thead>
+  <tbody striped table-hover>
+    <tr *ngFor="let i of tableData" #row (click)="onRowSelect(row)">
+      <td>{{ i.technology }}</td>
+      <td>{{ i.experience }}</td>
+    </tr>
+  </tbody>
+</table>
   ~~~
   `
 
@@ -159,7 +174,7 @@ export class TableComponent implements OnInit {
       <th text-direction="left">Experience</th>
     </tr>
   </thead>
-  <tbody striped table-hover>
+  <tbody>
     <tr>
       <td text-direction="right">C++</td>
       <td text-direction="left">5 years</td>
@@ -181,22 +196,22 @@ export class TableComponent implements OnInit {
   |  CSS Variables   | Type  | Default |
   |---|---|----|
   |  \`--ea-table-thead-th-color\`  |  Sets the table header text color | rgba(0, 0, 0, 1) |
-  |  \`--ea-table-thead-th-bgcolor\`  |  Sets the background color for the table header | rgba(255, 255, 255, 1) |
-  |  \`--ea-table-thead-th-border-color\`   |  Sets the table header border color | rgba(0, 0, 0, 0.5) |
-  | \` --ea-table-thead-th-text-align \`| Align text in the table header | \`center\` |
+  |  \`--ea-table-thead-background\`  |  Sets the background color for the table header | rgba(255, 255, 255, 1) |
+  |  \`--ea-table-thead-border-color\`   |  Sets the table header border color | rgba(0, 0, 0, 0.5) |
+  | \` --ea-table-thead-text-align \`| Align text in the table header | \`center\` |
   |---|---|----|
   | \` --ea-table-tbody-text-align\` | Align text in the row | \`center\` |
   |---|---|----|
-  | \`--ea-table-border-bottom-width\` | Sets the widht of the border bottom | 0.2em |
+  | \`--ea-table-border-bottom-width\` | Sets the widht of the border bottom | 0.3em |
   | \`--ea-table-border-bottom-width\` | Sets the widht of the border bottom | rgba(148, 148, 148, 0.616)|
   |---|---|----|
-  | \`--ea-table-padding-top-bottom\` | Sets the top and bottom padding | 10px|
-  | \`--ea-table-padding-right-left\` | Sets the right and left padding | 10px|
+  | \`--ea-table-padding-top-bottom\` | Sets the top and bottom padding | .7em|
+  | \`--ea-table-padding-right-left\` | Sets the right and left padding | .7em|
   |---|---|----|
   | \`--ea-table-tr-hover-cursor\` | Sets the hover cursor | pointer|
-  | \`--ea-table-tr-hover-bgcolor\` | Sets the hover background color | rgba(83, 245, 8, 0.5)|
+  | \`--ea-table-tr-hover-background\` | Sets the hover background color | rgba(83, 245, 8, 0.5)|
   |---|---|----|
-  | \`--ea-table-row-selected-bgcolor\` | Sets the selected background color | rgba(179, 194, 230, 1)|
+  | \`--ea-table-row-selected-background\` | Sets the selected background color | rgba(179, 194, 230, 1)|
   `
 
   ngOnInit() {}
