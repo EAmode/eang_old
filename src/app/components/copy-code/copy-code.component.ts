600b262eeb19ff6139ccdf0c44e53c8fbb20ff12
Alexander Romaniak <sanya.romanyak@gmail.com>
2019-08-02 20:00:20 +0300

    feat(component): component to copy code from markdown

---
+++ src/app/components/copy-code/copy-code.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core'
+import { CopyPasteService } from 'src/app/services/copy-paste.service'
+import { MarkdownComponent } from '@eamode/eang'
+
+@Component({
+  selector: 'eangio-copy-code',
+  templateUrl: './copy-code.component.html',
+  styles: [
+    `
+      div {
+        display: grid;
+      }
+
+      .copy-clipboard {
+        display: flex;
+        justify-content: space-around;
+      }
+
+      small {
+        text-align: center;
+        color: var(--ea-color-accent-1);
+      }
+    `
+  ]
+})
+export class CopyCodeComponent {
+  @Input() templateToCopy: MarkdownComponent
+  constructor(public copyPasteService: CopyPasteService) {}
+}
