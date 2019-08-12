48c0195188d404bc62485cfc8412c26adbf986c8
Sebastian <sebastian@kropponline.de>
2019-08-12 15:54:50 +0200

    feat(button): button styles changed to configurable CSS architecture
    Rounded buttons have been removed, as the same effect can be achieved by setting --ea-border-radius
    or more specific the --ea-button-border-radius.

--- src/app/feature/button/button.component.ts
+++ src/app/feature/button/button.component.ts
@@ -15,20 +15,18 @@ export class ButtonComponent implements OnInit {
   button = `
   ~~~html
   <button class="ea-button">Button</button>
-  <button class="ea-button" rounded>Rounded</button>
+  <button class="ea-button" active>Active</button>
+  <button class="ea-button" disabled>Disabled</button>
+  <button class="ea-button" primary>Primary</button>
   <button class="ea-button" outline>Outline</button>
-  <button class="ea-button" outline rounded>Outline Rounded</button>
   <button class="ea-button" flat>Flat</button>
-  <button class="ea-button" flat rounded>Flat Rounded</button>
   ~~~
   `
 
   tags = `
   ~~~html
-  <div class="ea-button" role="ea-button">Div</div>
-  <span class="ea-button" role="ea-button" rounded>Rounded</span>
-  <a class="ea-button" role="ea-button" outline>Outline</a>
-  <label class="ea-button" role="ea-button" flat rounded>Flat Rounded</label>
+  <button class="ea-button" style="--ea-button-border-radius: 1em">Button</button>
+  <button class="ea-button" style="--ea-button-background: var(--ea-color-success);">Success</button>
   ~~~
   `
 
@@ -40,35 +38,6 @@ export class ButtonComponent implements OnInit {
   <button class="ea-button" xl flat>[xl]</button>
   <button class="ea-button" xxl>[xxl]</button>
   ~~~
-  `
-
-  button_icon = `
-  ~~~html
-  <button class="ea-button"><span icon negative sign-in></span></button>
-  <button class="ea-button" rounded><span icon negative filter></span></button>
-  <button class="ea-button" rounded>Button <span icon negative filter></span></button>
-  <button class="ea-button" icon outline><span icon trash-2></span></button>
-  <button class="ea-button" icon outline rounded><span icon trash-2></span></button>
-  <button class="ea-button" outline rounded><span icon trash-2></span> Delete</button>
-  <button class="ea-button" icon flat><span icon filter></span></button>
-  <button class="ea-button" flat rounded><span icon filter></span></button>
-  <button class="ea-button" flat rounded>Filter <span icon filter></span></button>
-  ~~~
-  `
-
-  props = `
-  |  Attribute  | Description |
-  |---|---|
-  | outline  | Removes the background and has only an outline around the button |
-  | rounded  |  Sets the border radius to 8rem to allow the highest possible curve |
-  | flat | Removes all background and border for only :hover and :active effects |
-  | icon | Removes majority of the padding to handle icon buttons that are meant to have smaller sizes |
-  `
-
-  button_variables = `
-  |  Name  | Description |
-  |---|---|
-  | --ea-button-background  | Change button background |
   `
 
   icons = `
