f064c274e2c39332263f3099b467f1d5345871fc
Sebastian <sebastian@kropponline.de>
2021-01-23 17:50:22 -0500

    added themes

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = ctx => ({
+  map: ctx.options.map,
+  plugins: [
+    require('postcss-import')(),
+    require('postcss-mixins')(),
+    require('postcss-nested')(),
+    require('postcss-assets')({
+      basePath: 'themes',
+      relative: 'themes',
+    }),
+  ],
+});
